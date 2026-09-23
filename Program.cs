097372e9d79809099f76f9f6471b7e5c0742eb91
Dennis <fernandezmirandadennis27@gmail.com>
2026-09-22 22:01:33 -0500

    Req. 1: registra datos básicos de solicitud

--- Program.cs
+++ Program.cs
@@ -1,1 +1,25 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("==========================================");
+Console.WriteLine(" SISTEMA DE SOPORTE ACADÉMICO");
+Console.WriteLine("==========================================");
+
+Console.WriteLine("\nREGISTRO DE SOLICITUD");
+
+Console.Write("Código del estudiante: ");
+string codigoEstudiante = Console.ReadLine() ?? "";
+
+Console.Write("Nombre del estudiante: ");
+string nombre = Console.ReadLine() ?? "";
+
+Console.Write("Tipo de consulta: ");
+string tipoConsulta = Console.ReadLine() ?? "";
+
+Console.Write("Descripción de la solicitud: ");
+string descripcion = Console.ReadLine() ?? "";
+
+Console.WriteLine("\n------------------------------------------");
+Console.WriteLine("SOLICITUD REGISTRADA");
+Console.WriteLine("------------------------------------------");
+Console.WriteLine($"Código: {codigoEstudiante}");
+Console.WriteLine($"Nombre: {nombre}");
+Console.WriteLine($"Tipo de consulta: {tipoConsulta}");
+Console.WriteLine($"Descripción: {descripcion}");
